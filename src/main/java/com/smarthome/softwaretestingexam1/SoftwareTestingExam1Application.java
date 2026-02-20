cc9220d726608256ddf79b8f7aa3395edfe9560c
Aiden Cary <ACARY@cub.uca.edu>
2026-02-19 23:33:40 -0600

    Init commit

--- src/main/java/com/smarthome/softwaretestingexam1/SoftwareTestingExam1Application.java
+++ src/main/java/com/smarthome/softwaretestingexam1/SoftwareTestingExam1Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SoftwareTestingExam1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SoftwareTestingExam1Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareTestingExam1Application.class, args);
+    }
 
 }
--- src/test/java/com/smarthome/softwaretestingexam1/SoftwareTestingExam1ApplicationTests.java
+++ src/test/java/com/smarthome/softwaretestingexam1/SoftwareTestingExam1ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SoftwareTestingExam1ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
